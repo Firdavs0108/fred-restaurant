868a822428dc84dc3b5390f7936ddf60350bd8db
Firdavs Nazarov <fredrickuzb55@gmail.com>
2023-07-12 23:05:10 +0900

    Fredrick_restaurant

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,11 +1,48 @@
 import React from 'react';
+import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs';
 
-import './Intro.css';
+import { meal } from '../../constants';
+import {App__video, Video, App__video_overlay_circle } from './style.js';
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+
+const Intro = () => {
+
+  const [playVideo, setPlayVideo] = React.useState(false);
+  const vidRef = React.useRef();
+
+
+  return(
+
+  <App__video>
+    <Video
+         ref={vidRef}
+         src={meal}
+         type="video/mp4"
+         loop
+         controls={false}
+         muted 
+    /> 
+  <div className="app__video-overlay flex__center"
+   style={{position: 'absolute', inset: '0', background:'rgba(0,0,0,0.65)'}}>
+        <App__video_overlay_circle
+          onClick={() => {
+            setPlayVideo(!playVideo);
+            if (playVideo) {
+              vidRef.current.pause();
+            } else {
+              vidRef.current.play();
+            }
+          }}
+        >
+          {playVideo ? (
+            <BsPauseFill color="#fff" fontSize={30} />
+          ) : (
+            <BsFillPlayFill color="#fff" fontSize={30} />
+          )}
+        </App__video_overlay_circle>
+      </div>
+  </App__video>
+  )
+};
 
 export default Intro;
---
+++ src/container/Intro/style.js
@@ -0,0 +1,26 @@
+import { styled } from "styled-components";
+
+const App__video = styled.div`
+      height: 100vh;
+      position: relative;
+`
+
+const Video = styled.video`
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+`
+
+const App__video_overlay_circle = styled.div`
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+    border: 1px solid var(--color-golden);
+    cursor: pointer;
+`
+
+
+
+
+
+export {App__video, Video, App__video_overlay_circle}
