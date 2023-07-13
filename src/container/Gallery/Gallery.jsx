3212ff614f6c168e5f0f623aa1c2da2ba229029a
Firdavs Nazarov <fredrickuzb55@gmail.com>
2023-07-13 23:11:56 +0900

    Fredrick_restaurant

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -4,7 +4,7 @@ import { BsInstagram, BsArrowLeftShort, BsArrowRightShort } from 'react-icons/bs
 
 import { SubHeading } from '../../components';
 import { images } from '../../constants';
-import {App__gallery, App__gallery_content,App__gallery_images} from './style.js';
+import {App__gallery, App__gallery_content, App__gallery_images_arrows, Img,App__gallery_images, App__gallery_images_card, App__gallery_images_container} from './style.js';
 
 const Gallery = () =>  {
 
@@ -27,23 +27,25 @@ const Gallery = () =>  {
             <SubHeading title="Instagram" />
             <h1 className="headtext__cormorant">Photo Gallery</h1>
             <p className="p__opensans" style={{ color: '#AAAAAA', marginTop: '2rem' }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mu.</p>
-            <button style={{marginTop: '1rem'}} type="button" className="custom__button">View More</button>
+            <button style={{marginTop: '2rem'}} type="button" className="custom__button">View More</button>
           </App__gallery_content>
 
           <App__gallery_images>
-            <div className="app__gallery-images_container" ref={scrollRef}>
+            <App__gallery_images_container ref={scrollRef}>
             {[images.gallery01, images.gallery02, images.gallery03, images.gallery04].map((image, index) => (
-            <div className="app__gallery-images_card flex__center" key={`gallery_image-${index + 1}`}>
-              <img src={image} alt="gallery_image" />
-              <BsInstagram className="gallery__image-icon" />
-            </div>
+            <App__gallery_images_card key={`gallery_image-${index + 1}`}>
+              <Img src={image} alt="gallery_image" />
+             
+                <BsInstagram style={{color:'white', position:'absolute', fontSize:'2rem', opacity:'0', transition:'.5s ease', cursor:'pointer' }}/>
+    
+            </App__gallery_images_card>
           ))}
-            </div>
+            </App__gallery_images_container>
 
-            <div className="app__gallery-images_arrows">
-                 <BsArrowLeftShort className="gallery__arrow-icon" onClick={() => scroll('left')} />
-                 <BsArrowRightShort className="gallery__arrow-icon" onClick={() => scroll('right')} />
-            </div>
+            <App__gallery_images_arrows>
+                 <BsArrowLeftShort style={{color:'var(--color-golden)', fontSize:'2rem', cursor:'pointer', background:'var(--color-black)', borderRadius:'5px'}} className="gallery__arrow-icon" onClick={() => scroll('left')} />
+                 <BsArrowRightShort style={{color:'var(--color-golden)', fontSize:'2rem', cursor:'pointer', background:'var(--color-black)', borderRadius:'5px'}} className="gallery__arrow-icon" onClick={() => scroll('right')} />
+            </App__gallery_images_arrows>
           </App__gallery_images>
        </App__gallery>
      );
--- src/container/Gallery/style.js
+++ src/container/Gallery/style.js
@@ -5,6 +5,20 @@ const App__gallery = styled.div`
     flex-direction: row;
     background: var(--color-black);
     padding: 4rem 0 4rem 6rem;
+   
+    @media screen and (max-width: 1150px) 
+    {
+        flex-direction: column;
+    }
+
+    @media screen and (max-width: 850px) {
+        padding: 4rem 0 4rem 4rem;
+    }
+
+    @media screen and (max-width: 650px) {
+        padding: 4rem 0 4rem 2rem;
+    }  
+    
 `
 
 const App__gallery_content = styled.div`
@@ -15,15 +29,106 @@ const App__gallery_content = styled.div`
     flex-direction: column;
     min-width: 500px;
     padding-right: 2rem;
+
+    @media screen and (min-width: 2000px) {
+        min-width: 1000px;
+        padding-right: 4rem;
+    }
+
+    @media screen and (max-width: 650px) {
+        min-width: 100%;
+    }
+
+
 `
 
 
 const App__gallery_images = styled.div`
+    flex: 1;
+    display: flex;
+    flex-direction: row;
+    max-width: 50%;
+    position: relative;
+
+    @media screen and (max-width: 1150px) 
+    {
+        max-width: 100%;
+        margin: 5rem 0;
+    }
+
+`
+
+const App__gallery_images_container = styled.div`
+    display: flex;
+    flex-direction: row;
+    width: max-content;
+    overflow-x: scroll;
+    -ms-overflow-style: none;
+    scrollbar-width: none; 
+
+    &::-webkit-scrollbar {
+        display: none;
+    }
+`;
+
+const App__gallery_images_card = styled.div`
+    position: relative;
+    min-width: 301px;
+    height: 447px;
+    margin-right: 2rem;
+
+    @media screen and (min-width: 2000px) {
+        min-width: 400px;
+        height: 547px;
+    }
+
+    @media screen and (max-width: 650px) {
+        min-width: 240px;
+        height: 320px;
+    }
+`
+
+const Img = styled.img`
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    opacity: 1;
+    transition: 0.5s ease;
+
+    &:hover {
+        opacity: 0.35;
+    }
+`
+
+const App__gallery_images_arrows = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    padding: 0 1rem;
+    position: absolute;
+    bottom: 5%;
     
+
+`
+const Gallery__image_icon = styled.div`
+     position: absolute;
+    color: #fff;
+    font-size: 2rem;
+    opacity: 0;
+    transition: .5s ease;
+    cursor: pointer;
+    &:hover {
+        color: var(--color-white);
+    }
+
 `
 
 
-export {App__gallery, App__gallery_content,  App__gallery_images}
+
+
+export {App__gallery, App__gallery_images_arrows, Img, App__gallery_content, App__gallery_images_card, App__gallery_images_container,  App__gallery_images}
 
 
 //  5:30:01
