6384dcfa4202838c6e9e09e80cdc28013b242837
Firdavs Nazarov <fredrickuzb55@gmail.com>
2023-07-14 21:37:54 +0900

    Fredrick_restaurant

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -3,11 +3,15 @@ import React from 'react';
 import { FiFacebook, FiTwitter, FiInstagram } from 'react-icons/fi';
 import { FooterOverlay, Newsletter } from '../../components';
 
-import {A} from './style.js';
+import './Footer.css';
 
 const Footer = () => (
-  <div>
-    
+  <div className="app__footer section__padding" id="login">
+          <FooterOverlay />
+          <Newsletter />
+
+
+
   </div>
 );
 
